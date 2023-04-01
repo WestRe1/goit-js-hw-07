d4028472b47ea88e0a299490b0d0bc1d3d19af86
Jura Mazar <ura2003mazar@gmail.com>
2023-04-01 18:57:35 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+(function () {
+    const marcup = galleryItems.map(({preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" target="_self" href="${original}">
+    <img
+      class="gallery__image js-target"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+    gallery.insertAdjacentHTML('beforeend', marcup)
+})();
+
+gallery.addEventListener('click', onClick)
+
+function onClick(evt) {
+   evt.preventDefault();
+    if (!evt.target.classList.contains('js-target')) {
+        return;
+    }
+    const currentItem = evt.target.closest('.js-target')
+    const source = currentItem.dataset.source
+    
+
+    const data = galleryItems.find(({ original }) => original === source);
+    console.log(data)
+    
+    const instance = basicLightbox.create(`
+    <img src="${data.original}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+
+
+
+
